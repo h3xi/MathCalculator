e61c3a7858094a1a9567eb17786440585d6df943
Ilia Starovoitov <workplaceilfed@gmail.com>
2021-12-07 23:18:41 +0300

    первое обновление - создал основной проект, создал первое меню (выбор категории) + цикл (для дальнейшего выхода)

---
+++ MathCalculator/MathCalculator/Index.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	int UserOption, NotExit = true;
+    setlocale(LC_CTYPE, "Russian");
+    printf("Здравствуйте!\n");
+    Sleep(1000);
+    while (NotExit) {
+        printf("Выберите один из доступных вариантов:\n");
+        printf("1. Линейная Алгебра\n");
+        printf("2. Математический анализ\n");
+        printf("3. Выход\n");
+        cin >> UserOption;
+		switch (UserOption)
+		{
+		case 1:
+			printf("Редирект на матан\n"); //ссылка на модуль матана
+			break;
+		case 2:
+			printf("Редирект на алгебру\n"); //ссылка на модуль алгебры
+			break;
+		case 3:
+			printf("Выход....\n"); // ссылка на модуль выхода
+			NotExit = false;
+			break;
+		default:
+			printf("Ошибка, повторите ввод!\n");
+			break;
+		}
+    }
+}
