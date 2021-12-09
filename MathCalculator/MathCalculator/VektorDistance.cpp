137213a36b2ba625f540eeb2f7a549239a96a378
natasha <natalya.budchan@gmail.com>
2021-12-09 20:20:13 +0300

    Добавление готового модуля Вектора

---
+++ MathCalculator/MathCalculator/VektorDistance.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <cmath>
+#include  "MainAlgebraModule.h"
+
+using namespace std;
+
+void VektorDistance()
+{
+    setlocale(LC_CTYPE, "RU");
+    int A,B,C,D,A1,B1;
+    
+    printf("Введите значение A: ");
+    cin >> A;
+    printf("Введите значение B: ");
+    cin >> B;
+    printf("Введите значение C: ");
+    cin >> C;
+    printf("Введите значение D: ");
+    cin >> D;
+    A1 = abs(C - A);
+    B1 = abs(D - B);
+        printf("Расстояние между точками равно (%d, %d)\n", A1, B1);
+        system("pause");
+        MainAlgebraModule();
+
+    
+}
+
