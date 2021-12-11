973ee2894a53948c6925b678fdd84562bd169552
leon <ilyaleonovich04@gmail.com>
2021-12-11 22:13:34 +0300

    added files

--- MathCalculator/MathCalculator/MainAlgebraModule.cpp
+++ MathCalculator/MathCalculator/MainAlgebraModule.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MainAlgebraModule.h"
 #include "Matrix3x3.h"
+#include "Factorial.h"
 using namespace std;
 
 void MainAlgebraModule()
@@ -11,13 +12,16 @@ void MainAlgebraModule()
 		printf("\033c");
 		printf("Добро пожаловать в модуль Линейной Алгебры!\nВыберите один из доступных пунктов:\n");
 		printf("1.Посчитать определитель матрицы 3*3\n");
-		printf("2.Выход\n");
+		printf("2.Подсчитать факториал целого числа\n");
+		printf("3.Выход\n");
 		cin >> UserChoise;
 		switch (UserChoise)
 		{
 		case 1:
 			Matrix3x3();
 		case 2:
+			Factorial();
+		case 3:
 			exited = false;
 			break;
 		}
