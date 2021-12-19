03d04d0072501f7d7907272eb74819c0f4bf086e
leon <ilyaleonovich04@gmail.com>
2021-12-19 16:07:32 +0300

    added verification to MatrixSummation.cpp

--- MathCalculator/MathCalculator/MatrixSummation.cpp
+++ MathCalculator/MathCalculator/MatrixSummation.cpp
@@ -7,40 +7,73 @@ using namespace std;
 int MatrixSummation() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int rows, columns;
+	char rows[MAX_MATRIX_SIZE], columns[MAX_MATRIX_SIZE];	
+	char Element[MAX_SIZE];
+	int irows, icolumns;
 	int i, j, firstMatrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE], secondMatrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE], resultMatrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE];
 	printf("Введите количество строк первой матрицы: \n");
 	cin >> rows;
+	if (static_cast<int>(rows[0]) > 57 || static_cast<int>(rows[0]) < 48) {
+		printf("Ошибка - введен неправильный символ...\n");
+		system("pause");
+		return 0;
+	}
+	irows = atoi(rows);
 	printf("Введите количество столбцов первой матрицы: \n");
 	cin >> columns;
+	if (static_cast<int>(columns[0]) > 57 || static_cast<int>(columns[0]) < 48) {
+		printf("Ошибка - введен неправильный символ...\n");
+		system("pause");
+		return 0;
+	}
+	icolumns = atoi(columns);
 	// РАЗМЕРНОСТЬ ВТОРОЙ МАТРИЦЫ БУДЕТ ТАКАЯ ЖЕ
 	printf("Введите элементы первой матрицы: \n");
-	for (i = 0; i < rows; i++)
-		for (j = 0; j < columns; j++)
-			cin >> firstMatrix[i][j];
+	for (i = 0; i < irows; i++) {
+		for (j = 0; j < icolumns; j++) {
+			cin >> Element;
+			if (static_cast<int>(Element[0]) > 57 || static_cast<int>(Element[0]) < 48) {
+				printf("Ошибка - введен неправильный символ....\n");
+				system("pause");
+				return 0;
+			}
+			else { 
+				firstMatrix[i][j] = atoi(Element); 
+			}
+		}
+	}
 	printf("Первая матрица: \n");
-	for (i = 0; i < rows; i++) {
-		for (j = 0; j < columns; j++)
+	for (i = 0; i < irows; i++) {
+		for (j = 0; j < icolumns; j++)
 			cout << firstMatrix[i][j] << "\t";
 		cout << endl;
 	}
-
 	printf("Введите элементы второй матрицы: \n");
-	for (i = 0; i < rows; i++)
-		for (j = 0; j < columns; j++)
-			cin >> secondMatrix[i][j];
+	for (i = 0; i < irows; i++) {
+		for (j = 0; j < icolumns; j++) {
+			cin >> Element;
+			if (static_cast<int>(Element[0]) > 57 || static_cast<int>(Element[0]) < 48) {
+				printf("Ошибка - введен неправильный символ....\n");
+				system("pause");
+				return 0;
+			}
+			else {
+				secondMatrix[i][j] = atoi(Element);
+			}
+		}
+	}
 	printf("Вторая матрица: \n");
-	for (i = 0; i < rows; i++) {
-		for (j = 0; j < columns; j++)
+	for (i = 0; i < irows; i++) {
+		for (j = 0; j < icolumns; j++)
 			cout << secondMatrix[i][j] << "\t";
 		cout << endl;
 	}
 	printf("Результат сложения матриц: \n");
-	for (i = 0; i < rows; i++)
-	    for (j = 0; j < columns; j++)
+	for (i = 0; i < irows; i++)
+	    for (j = 0; j < icolumns; j++)
 		resultMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
-	for (i = 0; i < rows; i++) {
-		for (j = 0; j < columns; j++)
+	for (i = 0; i < irows; i++) {
+		for (j = 0; j < icolumns; j++)
 			cout << resultMatrix[i][j] << "\t";
 		cout << endl;
 	}
